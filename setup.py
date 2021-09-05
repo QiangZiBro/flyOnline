fa39b397a9d8f47d74eeca6423e2e70e0b54f1d6
QiangZiBro <qiangzibro@gmail.com>
2021-09-06 02:18:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 # -*- coding: UTF-8 -*-
+import sys
+import os
 from setuptools import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+elif sys.argv[-1] == 'clean':
+    import shutil
+    if os.path.isdir('build'):
+        shutil.rmtree('build')
+    if os.path.isdir('dist'):
+        shutil.rmtree('dist')
+    if os.path.isdir('__pycache__'):
+        shutil.rmtree('__pycache__')
+    if os.path.isdir('fly_online.egg-info'):
+        shutil.rmtree('fly_online.egg-info')
 
 setup(
     name='fly_online',
-    version='0.0.5',
+    version='0.0.6',
     author='QiangZiBro',
     author_email='qiangzibro@gmail.com',
     url="https://github.com/QiangZiBro/flyOnline",
